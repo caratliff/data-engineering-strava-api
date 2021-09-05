b89eeab37cf2fc7fb73e263250a6b195dd25a86f
rashaad.ratliffbrown <rashaad.ratliffbrown@duke.edu>
2021-09-05 04:38:19 +0000

    think i fixed it

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+    
+@app.get("/")
+async def displayData():
+    return{"Activities": data}
+    
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
